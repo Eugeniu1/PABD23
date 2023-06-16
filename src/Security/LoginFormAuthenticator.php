b2e45408664c506e9177f3debb523ded433c2c15
Eugeniu1 <maros_08@mail.ru>
2023-06-16 17:54:02 +0300

    eugeniu1

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -15,8 +15,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
-{
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator{
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
